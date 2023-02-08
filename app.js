b3492e3aa03911e88161d7b7dd445dd195eb3c08
Andres felipe Candela <andrescandela698@gmail.com>
2023-02-08 15:02:13 -0500

    Se crearon los estilos

---
+++ app.js
@@ -0,0 +1,19 @@
+const nota1 = document.getElementById("nota1");
+const nota2 = document.getElementById("nota2");
+const nota3 = document.getElementById("nota3");
+
+if(nota1,nota2,nota3 < 2.0){
+    print("Perdio la asignatura");
+} 
+
+else if(nota1,nota2,nota3 > 2.1 && nota1,nota2,nota3 < 3.4){
+    print("Perdio la asignatura pero tiene oportunidad de recuperar")
+}
+
+else if(nota1,nota2,nota3 > 3.5 && nota1,nota2,nota3 < 4.5){
+    print("Aprobo de manera satisfactoria")
+}
+
+else if(nota1,nota2,nota3 > 4.6 && nota1,nota2,nota3 < 5.0){
+    print("Aprobo y sera reconocido por la plataforma como un aprendiz excelente o con honores")
+}
